1efaad58a154b7f59ea0b533d3ad534cad91e6d7
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-07-08 17:03:24 -0700

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    has_many :projects
+    has_many :user_projects
     # has_many :user_project
     # has_many :projects, through: :user_project
+    has_many :user_projects, dependent: :delete_all
     validates :username, uniqueness: { case_sensitive: false }
     has_secure_password
 end
